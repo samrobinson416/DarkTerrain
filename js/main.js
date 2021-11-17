dd011d34675009866a4b5ba44e3cbecffa2acab3
samrobinson416 <89624485+samrobinson416@users.noreply.github.com>
2021-11-17 08:15:00 -0700

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//function to start the Leaflet map
+function createMap(){
+
+    //Varibles needed for running the funcation
+    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' +' <a href="http://mapbox.com">Mapbox</a> | <a href="https://sounny.github.io/">Sounny Slitine</a>';
+    
+    var apitoken = 'pk.eyJ1Ijoic2Ftcm9iaW5zb240MTYiLCJhIjoiY2t2enV2N2M1MWc5NTJudDN4bzh3ZHYzZSJ9.12Rsgw92c40PdfUe3IBRKQ' //Enter your API Token - go to 'https://www.mapbox.com/install/' to yours */
+    
+    var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}'; //URL used for Stanard MaxBox Styles
+    
+    var mbStyleUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}'; //URL used for Custom MapBox Styles
+    
+    
+    //Variables for the different basemaps
+    var darkTerrain = L.tileLayer(mbStyleUrl, {id: 'samrobinson416/ckvzuyocj4w3e14qa6zimg70k', token: apitoken,  attribution: mbAttr});
+    
+    var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', token: apitoken, attribution: mbAttr});
+    
+    var dark  = L.tileLayer(mbUrl, {id: 'mapbox.dark', token: apitoken,  attribution: mbAttr});
+    
+    var outdoors = L.tileLayer(mbUrl, {id: 'mapbox.outdoors', token: apitoken,  attribution: mbAttr});
+    
+   
+    
+    //create the map*/
+    var map = L.map('map', {
+        center: [31.00, -5],//Coordinated to center the map
+        zoom: 6, //zoom level
+        layers:darkTerrain //default base
+    });
+    
+    //create the basemap control layer*/
+    var baseLayers = {
+		"Outdoors": outdoors,
+        "Grayscale": grayscale,
+		"Darkscale": dark,
+        "Dark Terrain": darkTerrain
+    };
+    
+    L.control.layers(baseLayers).addTo(map);
+};
+
+//calling the funcation
+$(document).ready(createMap);
